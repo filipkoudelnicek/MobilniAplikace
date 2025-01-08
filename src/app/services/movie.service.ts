60d9531b366c70436e8a7c39a84a1a16c2e11c61
filipkoudelnicek@seznam.cz <62012471+filipkoudelnicek@users.noreply.github.com>
2025-01-08 14:46:07 +0100

    movie service a gitignor

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MovieService {
+  private apiUrl = 'https://www.omdbapi.com/';
+
+  constructor(private http: HttpClient) {}
+
+  // Metoda pro vyhledání filmů
+  searchMovies(query: string) {
+    return this.http.get(`${this.apiUrl}`, {
+      params: {
+        apikey: environment.omdbApiKey,
+        s: query,
+      },
+    });
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  tmdbApiKey: 'VAŠE_API_KLÍČE',
 };
