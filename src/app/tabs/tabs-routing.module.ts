1a2a9d23ba56c5f9431bd17a0b18f6b4e71aaf15
filipkoudelnicek@seznam.cz <62012471+filipkoudelnicek@users.noreply.github.com>
2025-01-08 12:12:26 +0100

    Změna v URL pro taby

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,27 +8,27 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'search',
         loadComponent: () => import('../tab1/tab1.page').then(m => m.Tab1Page)
       },
       {
-        path: 'tab2',
+        path: 'favorites',
         loadComponent: () => import('../tab2/tab2.page').then(m => m.Tab2Page)
       },
       {
-        path: 'tab3',
+        path: 'settings',
         loadComponent: () => import('../tab3/tab3.page').then(m => m.Tab3Page)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/search',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/search',
     pathMatch: 'full'
   }
 ];
